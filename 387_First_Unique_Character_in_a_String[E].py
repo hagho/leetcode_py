3f7b739da4ff0210a304488cb6e3451d0615f901
hagho <869158286@qq.com>
2017-03-22 20:52:12 +0800

    简单题，用到列表解析

--- 387_First_Unique_Character_in_a_String[E].py
+++ 387_First_Unique_Character_in_a_String[E].py
@@ -4,4 +4,4 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        return min( (s.find(c) for c in string.ascii_lowercase if s.count(c) == 1) or [-1])
+        return min( [s.find(c) for c in string.ascii_lowercase if s.count(c) == 1] or [-1])
