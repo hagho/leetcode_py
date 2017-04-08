ed16394193891c51d8da40ec68d0527e093f7b42
hagho <869158286@qq.com>
2017-04-08 11:27:46 +0800

    自定义排序函数，要注意False的值为0，而自定义cmp中，返回值应该为-1，所以不能用>,<,==这类符号进行编写cmp函数

--- 406_Queue_Reconstruction_by_Height[M].py
+++ 406_Queue_Reconstruction_by_Height[M].py
@@ -4,4 +4,14 @@ class Solution(object):
         :type people: List[List[int]]
         :rtype: List[List[int]]
         """
-        
+        def mycmp(a, b):
+            if a[0] == b[0]:
+                return a[1] - b[1]
+            else:
+                return b[0] - a[0]
+        people.sort(mycmp)
+        #print people
+        res = []
+        for i in people:
+            res.insert(i[1], i)
+        return res
