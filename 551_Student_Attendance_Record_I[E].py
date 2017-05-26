9fadf27b63b7840c3f31833fdd421eb458c65bbb
huangchao <869158286@qq.com>
2017-05-26 10:33:44 +0800

    简单题，找一个字符串中出现的特殊子串或某字符的出现次数，主要用到字符串的count函数和in关键字

---
+++ 551_Student_Attendance_Record_I[E].py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def checkRecord(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        if s.count('A') > 1:
+            return False
+        if "LLL" in s:
+            return False
+        return True
