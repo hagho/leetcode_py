c3eaa63a760ee581631d58d2e40b6d3a89b66d08
huangchao <869158286@qq.com>
2018-04-15 11:46:08 +0800

    最长回文子串，该题就是遍历一遍字符串，在字符串中的每一个位置校正最长的回文子串，下标处理有点难理解

---
+++ 005_Longest_Palindromic_Substring[M].py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        if not s:
+            return ""
+        start = 0
+        maxlen = 1
+        for i in xrange(len(s)):
+            if i - maxlen >= 1 and s[i - maxlen - 1: i + 1] == s[i - maxlen - 1: i + 1][::-1]:
+                start = i - maxlen - 1
+                maxlen += 2
+                continue
+            if i - maxlen >= 0 and s[i - maxlen: i + 1] == s[i - maxlen: i + 1][::-1]:
+                start = i - maxlen
+                maxlen += 1
+        return s[start: start + maxlen]
